37862b64c7e7f32f96df363219f263e743897376
c_du <102283742+charlottedu1031@users.noreply.github.com>
2024-06-21 13:45:02 -0400

    First vite project ste up

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+	return (
+		<header className="header">
+
+			<img className="header-image" src="/images/troll-face.png" />
+			<h1 className="header-title">Meme Generator</h1>
+
+			<h2 className="header-project" >React Course - Project 3</h2>
+		</header>
+	)
+}
---
+++ src/components/Meme.jsx
@@ -0,0 +1,11 @@
+export default function Meme() {
+	return (
+		<form>
+			<input>
+			</input>
+			<input>
+			</input>
+			<button>Get a new meme image</button>
+		</form>
+	)
+}
