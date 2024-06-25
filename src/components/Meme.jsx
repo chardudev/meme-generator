45aa87117d4f58cb4b75df5bba3366dac3d9dfeb
c_du <102283742+charlottedu1031@users.noreply.github.com>
2024-06-24 22:34:58 -0400

    used state for image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,39 @@
+import memesData from "../../memesData.js"
+import React from 'react';
+
 export default function Meme() {
+	const [memeImage, setMemeImage] = React.useState()
+
+	function getMemeImage() {
+		const memesArray = memesData.data.memes
+		const randomNumber = Math.floor(Math.random() * memesArray.length)
+		setMemeImage(memesArray[randomNumber].url)
+	}
+
 	return (
-		<form>
-			<input>
-			</input>
-			<input>
-			</input>
-			<button>Get a new meme image</button>
-		</form>
+		<main>
+			<div className="form">
+				<div>
+					<label>Top Text:
+						<input
+							className="form-input"
+							type="text"
+							placeholder="Shut up">
+						</input>
+					</label>
+				</div>
+				<div>
+					<label>Bottom Text:
+						<input
+							className="form-input"
+							type="text"
+							placeholder="Take my money">
+						</input>
+					</label>
+				</div>
+				<button className="form-button" onClick={getMemeImage}>Get a new meme image</button>
+			</div>
+			<img src={memeImage} className="meme-image" />
+		</main>
 	)
 }
